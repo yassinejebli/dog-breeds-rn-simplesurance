9c26e7d9aa613cc25d4606b68c458bafda696a01
yassinejebli <jebli.yassine@gmail.com>
2019-09-23 19:42:53 +0300

    Add loading indicator for PersistGate component

--- App.tsx
+++ App.tsx
@@ -3,17 +3,26 @@ import { Provider } from 'react-redux'
 import {PersistGate} from "redux-persist/integration/react";
 import Router from "./router/router";
 import {persistor, store} from "./store/store";
+import styled from 'styled-components/native';
 
 const App = () => {
 
     return (
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
+            <PersistGate loading={<StyledActivityIndicator />} persistor={persistor}>
                 <Router />
             </PersistGate>
         </Provider>
     );
 };
 
+const StyledActivityIndicator = styled.ActivityIndicator.attrs({
+    size: 'large',
+    color: '#0000ff'
+})`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+`;
 
 export default App;
